f8a08f691fa4255ec9579e0120a833b444fc4b35
Shaunak Juvekar <shaunakjuvekar@Shaunaks-MacBook-Air.local>
2023-03-08 13:11:53 -0500

    application code

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_NT7E8Q1Kz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '24voic4kcb1pbqsqetd33tqstu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://fe9ny15fi2.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
